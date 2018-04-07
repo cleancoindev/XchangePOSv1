6b9d4d6933812754f199ea13c4d02b8ac25a08da
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2018-04-06 16:25:32 -1000

    Add files via upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Xchange-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "XchangeURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
